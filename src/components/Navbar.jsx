9dafb3c87052996d74b7fc483e81e76124d02cba
Aayush1805 <aayushvj9876@gmail.com>
2025-02-25 00:56:53 +0530

    changed assets location

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <>
       <div className='flex border space-x-8 items-center pl-2 py-2'>
-        <img className='w-[60px]' src='src/movielogo.webp' alt='Nothing to show' />
+        <img className='w-[60px]' src='/movielogo.webp' alt='Nothing to show' />
 
         <Link to='/' className='text-blue-400 text-xl font-medium'>Home</Link>
         <Link to='/watchlist' className='text-blue-400 text-xl font-medium' >Watch List</Link>
